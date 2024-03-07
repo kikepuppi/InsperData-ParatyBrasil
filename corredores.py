1889595453a194256f22fca0e6627a2d7478db83
kikepuppi <kikepuppi@gmail.com>
2024-03-06 23:25:25 -0300

    segunda opcao de funcao para pegar infos (aprox 40s mais rapida)

--- corredores.py
+++ corredores.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def corredor_info(link: str):
+def corredor_info(uri: str):
     dic = {}
+    link = f'https://utmb.world/en/runner/{uri}'
     url = str(link)
     page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 86.0.4240.198Safari / 537.36'})
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,3 +19,18 @@ def corredor_corridas(uri:str):
     url_corredor = f'https://utmb.world/_next/data/AZM8Mw2pZpHJ-hmhE6gko/en/runner/{uri}.json?runner={uri}'
     info = requests.get(url_corredor)
     return pd.DataFrame(info.json()["pageProps"]["results"]["results"])
+
+def infos(uri:str):
+    url_corredor = f'https://utmb.world/_next/data/AZM8Mw2pZpHJ-hmhE6gko/en/runner/{uri}.json?runner={uri}'
+    info = requests.get(url_corredor)
+    dict_about = {"Club":info.json()["pageProps"]["club"],
+                  "Team":info.json()["pageProps"]["team"],
+                  "Sponsor":info.json()["pageProps"]["sponsor"],
+                  "Description":info.json()["pageProps"]["description"],
+                  "Website":info.json()["pageProps"]["website"],
+                  "Geral":info.json()["pageProps"]["performanceIndexes"][0]["index"],
+                  "20K":info.json()["pageProps"]["performanceIndexes"][1]["index"],
+                  "50K":info.json()["pageProps"]["performanceIndexes"][2]["index"],
+                  "100K":info.json()["pageProps"]["performanceIndexes"][3]["index"],
+                  "100m":info.json()["pageProps"]["performanceIndexes"][4]["index"]}
+    return dict_about
