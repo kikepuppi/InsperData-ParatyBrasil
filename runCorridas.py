34d4ce92a2518d724e1a07f4334917616f9890c8
iara.vsp <iaravsp@al.insper.edu.br>
2024-04-10 11:25:45 -0300

    sql corridas

--- runCorridas.py
+++ runCorridas.py
@@ -2,7 +2,7 @@ from corridas import Corridas
 
 def main():
     webscraping = Corridas()
-    create = webscraping.create(5)
+    create = webscraping.create(1000)
     webscraping.run(create[0], create[1], create[2])
 
 if __name__ == '__main__':
