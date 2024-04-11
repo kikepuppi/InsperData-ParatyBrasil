1f6e376d8f1f4c6db6e5749d04b5934a529eee38
kikepuppi <kikepuppi@gmail.com>
2024-04-10 22:36:46 -0300

    tabela parcial

--- runCorredores.py
+++ runCorredores.py
@@ -3,7 +3,7 @@ from corredores import Corredores
 def main():
     webscraping = Corredores()
     create = webscraping.create()
-    webscraping.run(create[0], create[1], create[2], create[3], 10)
+    webscraping.run(create[0], create[1], create[2], create[3], 1000)
 
 if __name__ == '__main__':
     main()
